c3343503785453c93b86cfccb5ea25a8308ab0c5
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-12-26 22:51:01 +0530

    routes

--- http-server/routers/routes.go
+++ http-server/routers/routes.go
@@ -2,18 +2,27 @@ package routers
 
 import (
 	"github/http-server/controllers"
+	"github/http-server/middleware"
 
 	"github.com/gorilla/mux"
 )
 
 func SetupRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/task/{id}", controllers.GetTaskByIdController).Methods("GET")
-	router.HandleFunc("/tasks", controllers.GetTasksController).Methods("GET")
-	router.HandleFunc("/tasks", controllers.CreateTaskListController).Methods("POST")
-	router.HandleFunc("/task", controllers.CreateTaskController).Methods("POST")
-	router.HandleFunc("/tasks", controllers.CreateTasksController).Methods("POST")
-	router.HandleFunc("/task/{id}", controllers.DeleteTaskByIdController).Methods("DELETE")
-	router.HandleFunc("/task/{id}", controllers.UpdateTaskByIdController).Methods("PUT")
+
+	userRouter := router.PathPrefix("/api/v1/user").Subrouter()
+
+	userRouter.HandleFunc("/register", controllers.RegisterUserController).Methods("POST")
+	userRouter.HandleFunc("/login", controllers.LoginUserController).Methods("POST")
+
+	userRouter.Use(middleware.AuthMiddleware)
+
+	userRouter.HandleFunc("/task/{id}", controllers.GetTaskByIdController).Methods("GET")
+	userRouter.HandleFunc("/tasks", controllers.GetTasksController).Methods("GET")
+	userRouter.HandleFunc("/tasks", controllers.CreateTaskListController).Methods("POST")
+	userRouter.HandleFunc("/task", controllers.CreateTaskController).Methods("POST")
+	userRouter.HandleFunc("/tasks", controllers.CreateTasksController).Methods("POST")
+	userRouter.HandleFunc("/task/{id}", controllers.DeleteTaskByIdController).Methods("DELETE")
+	userRouter.HandleFunc("/task/{id}", controllers.UpdateTaskByIdController).Methods("PUT")
 	return router
 }

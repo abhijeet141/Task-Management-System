70ca43773b95387a5dff474943382c910bd14d7d
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-12-26 11:11:48 +0530

    routes

--- http-server/routers/routes.go
+++ http-server/routers/routes.go
@@ -10,7 +10,10 @@ func SetupRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/task/{id}", controllers.GetTaskByIdController).Methods("GET")
 	router.HandleFunc("/tasks", controllers.GetTasksController).Methods("GET")
+	router.HandleFunc("/tasks", controllers.CreateTaskListController).Methods("POST")
 	router.HandleFunc("/task", controllers.CreateTaskController).Methods("POST")
 	router.HandleFunc("/tasks", controllers.CreateTasksController).Methods("POST")
+	router.HandleFunc("/task/{id}", controllers.DeleteTaskByIdController).Methods("DELETE")
+	router.HandleFunc("/task/{id}", controllers.UpdateTaskByIdController).Methods("PUT")
 	return router
 }

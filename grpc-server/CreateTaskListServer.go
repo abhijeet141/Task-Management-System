2db97d0352097b11a58dc918b87b842f9781e72b
abhijeet141 <sinhaabhijeet.smith@gmail.com>
2024-12-26 11:12:16 +0530

    create tasks list

---
+++ grpc-server/CreateTaskListServer.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	pb "github/grpc-server/proto/generated"
+	"io"
+	"log"
+	"time"
+
+	"github.com/beego/beego/v2/client/orm"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (t *TaskManagementServer) CreateTaskList(stream pb.TaskManagementService_CreateTaskListServer) error {
+	o := orm.NewOrm()
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("error receiving task: %v", err)
+		}
+		createdAt := req.CreatedAt
+		if createdAt == nil {
+			createdAt = timestamppb.New(time.Now())
+		}
+		istLocation, err := time.LoadLocation("Asia/Kolkata")
+		if err != nil {
+			return fmt.Errorf("error loading location %v", err)
+		}
+
+		createdAtTimeIst := createdAt.AsTime().In(istLocation).Format("2006-01-02 15:04:05")
+		parsedTime, err := time.Parse("2006-01-02 15:04:05", createdAtTimeIst)
+
+		if err != nil {
+			return fmt.Errorf("error parsing formatted time: %v", err)
+		}
+		dbTask := Task{
+			Title:       req.Title,
+			Description: req.Description,
+			Status:      req.Status,
+			CreatedAt:   parsedTime,
+		}
+
+		_, err = o.Insert(&dbTask)
+		if err != nil {
+			return fmt.Errorf("failed to insert task %w", err)
+		}
+		log.Printf("Got request with task title: %v", req.Title)
+
+		err = stream.Send(&pb.Task{
+			Title:       req.Title,
+			Description: req.Description,
+			Status:      req.Status,
+			CreatedAt:   req.CreatedAt,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to send task %w", err)
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
---
+++ http-server/controllers/create_task_list_controller.go
@@ -0,0 +1,99 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	grpcclient "github/http-server/grpc-client"
+	pb "github/http-server/proto/generated"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func CreateTaskListController(w http.ResponseWriter, r *http.Request) {
+	var tasks pb.TaskList
+	TaskId := 0
+	err := json.NewDecoder(r.Body).Decode(&tasks)
+	if err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+	client, err := grpcclient.TaskManagementClient()
+	if err != nil {
+		http.Error(w, "Failed to connect to gRPC service", http.StatusInternalServerError)
+		return
+	}
+	stream, err := client.CreateTaskList(context.Background())
+	if err != nil {
+		http.Error(w, "Failed to create gRPC stream", http.StatusInternalServerError)
+		return
+	}
+
+	tasksItem := make(map[string]interface{})
+	var wg sync.WaitGroup
+	var m sync.Mutex
+
+	istLocation, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		http.Error(w, "error loading location %v", http.StatusInternalServerError)
+		return
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("error while streaming: %v", err)
+				return
+			}
+			createdTime := res.CreatedAt.AsTime().In(istLocation).Format("2006-01-02 15:04:05")
+			parsedTime, err := time.Parse("2006-01-02 15:04:05", createdTime)
+			if err != nil {
+				http.Error(w, "error parsing formatted time", http.StatusInternalServerError)
+				return
+			}
+			m.Lock()
+			tasksItem[strconv.Itoa(TaskId)] = map[string]interface{}{
+				"title":       res.Title,
+				"description": res.Description,
+				"status":      res.Status,
+				"created_at":  parsedTime,
+			}
+			TaskId++
+			m.Unlock()
+		}
+	}()
+	for _, task := range tasks.Task {
+		err := stream.Send(&pb.Task{
+			Title:       task.Title,
+			Description: task.Description,
+			Status:      task.Status,
+		})
+		if err != nil {
+			log.Fatalf("Error while sending task details: %v", err)
+			continue
+		}
+		log.Printf("Sent Task: %v", task)
+		time.Sleep(1 * time.Second)
+	}
+	stream.CloseSend()
+	wg.Wait()
+	m.Lock()
+	defer m.Unlock()
+	response, err := json.MarshalIndent(tasksItem, " ", "\t")
+	if err != nil {
+		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
